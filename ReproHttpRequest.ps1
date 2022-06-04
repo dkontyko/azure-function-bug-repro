55178d5c9b79bd9935189b447585258b913046cf
dkontyko <33853225+dkontyko@users.noreply.github.com>
2022-06-04 10:50:45 -0400

    Create ReproHttpRequest.ps1

---
+++ ReproHttpRequest.ps1
@@ -0,0 +1,16 @@
+# Test PDF: https://armypubs.army.mil/pub/eforms/DR_a/ARN31947-DA_FORM_4187-002-EFILE-3.pdf
+$fileContents = Get-Content "A4187.pdf" -AsByteStream
+$port = 55872
+
+# fails with application/pdf
+# succeeds with application/octet-stream
+$headers = @{
+    "Content-Type" = "application/pdf"
+    "Accept" = "application/xml"
+}
+
+$response = Invoke-WebRequest -Uri "http://localhost:$port/api/HttpExample" -Method Post -Headers $headers -Body $fileContents
+
+Write-Output $response
+
+Write-Output $response.Content
